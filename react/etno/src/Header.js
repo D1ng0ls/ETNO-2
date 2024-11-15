ef8f8050b9d13c19bcb93ce66690fad821452829
d1ng0ls <4b4c4t3666@gmail.com>
2024-11-15 00:35:50 -0300

    inicio react

---
+++ react/etno/src/Header.js
@@ -0,0 +1,79 @@
+import logo from './media/global/logo-branco.png';
+
+function Header() {
+  return (
+    <header className="header">
+        <div className="header-logo">
+            <a
+                className="Header-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img src={logo} className="ETNO-logo" alt="logo" />
+            </a>
+        </div>
+        <nav className="header-menu">
+            <ul>
+                <li>
+                    <a
+                        className="Header-link"
+                        href="index.html"
+                    >
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className="Header-link"
+                        href="noticias.html"
+                    >
+                        Notícias
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className="Header-link"
+                        href="eventos.html"
+                    >
+                        Eventos
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className="Header-link"
+                        href="posts.html"
+                    >
+                        Posts
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className="Header-link"
+                        href="sobre.html"
+                    >
+                        Sobre
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className="Header-link"
+                        href="faq.html"
+                    >
+                        FAQ
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+
+
+// <div class="menu" onclick="burgermenu()">
+//     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+//         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+//     </svg>
+// </div>
+}
+
+export default Header;
---
+++ react/etno/src/Home.js
@@ -0,0 +1,29 @@
+function Home() {
+  return (
+    <main>
+        <section className="home" id="home">
+            <article className="home-conteudo">
+                <h1>Comissão<br></br> ETNO - Birigui</h1>
+                <h3>IFSP - Campus Birigui</h3>
+            </article>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+            </svg>
+        </section>  
+    </main> 
+  );
+}
+
+export default Home;
+
+
+
+{/* <article class="home-conteudo">
+                <h1>Comissão<br>ETNO - Birigui</h1>
+                <h3>IFSP - Campus Birigui</h3>
+            </article>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+                <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
+            </svg> */}
